381a19357f7e49f76b2b5e4b127a8ba49f0aa2d6
Kathrin Holzmann <lelith@gmx.de>
2018-05-03 17:47:15 +0200

    labels

--- src/components/FunGraph.js
+++ src/components/FunGraph.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import * as d3 from "d3";
 
+
 export default class extends Component {
   componentDidMount() {
     d3.json("data/tweets.json", (error, data) => {
@@ -19,19 +20,21 @@ export default class extends Component {
     });
 
     const maxImpact = d3.max(incomingData, d => d.impact);
-    var startEnd = d3.extent(incomingData, d => d.tweetTime);
-    var timeRamp = d3.scaleTime().domain(startEnd).range([20,480]);
-    var yScale = d3.scaleLinear().domain([0,maxImpact]).range([0,460]);
+    const startEnd = d3.extent(incomingData, d => d.tweetTime);
+    const timeRamp = d3.scaleTime().domain(startEnd).range([20,480]);
+    const yScale = d3.scaleLinear().domain([0,maxImpact]).range([0,460]);
 
-    var radiusScale = d3.scaleLinear()
+    const radiusScale = d3.scaleLinear()
     .domain([0,maxImpact]).range([1,20]);
 
-    var colorScale = d3.scaleLinear()
+    const colorScale = d3.scaleLinear()
     .domain([0,maxImpact]).range(["white","purple"])
+
+    const filteredData = incomingData.filter(d => d.impact > 0)
 
     var tweetG =  d3.select(this.refs.fungraph)
       .selectAll("g")
-      .data(incomingData)
+      .data(incomingData, JSON.stringify)
       .enter()
       .append("g")
       .attr("transform", d =>
@@ -44,7 +47,14 @@ export default class extends Component {
         .style("stroke-width", "1px");
 
     tweetG.append("text")
-      .text(d => `${d.user}-${d.tweetTime.getHours()}`);
+          .text(d => `${d.user}-${d.tweetTime.getHours()}`);
+
+    d3.selectAll("circle")
+      .data(filteredData, d =>JSON.stringify(d))
+      .exit()
+      .remove();
+
+
   }
 
   render() {
