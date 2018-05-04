ca8259733e5bbad2e4f5d54edcd54d6e30654e57
Kathrin Holzmann <lelith@gmx.de>
2018-05-04 16:26:30 +0200

    fancy styling and hovering

--- src/components/FunGraph.js
+++ src/components/FunGraph.js
@@ -29,51 +29,57 @@ export default class extends Component {
   }
 
   dataViz(incomingData) {
-    d3.select(this.refs.fungraph)
-      .append("g")
-      .attr("id", "teamsG")
-      .attr("transform", "translate(150,150)")
-      .selectAll("g")
-      .data(incomingData)
-      .enter()
-      .append("g")
-      .attr("class", "overallG")
-      .attr("transform", (d,i) => "translate(" + (i * 50) + ", 0)");
+    d3.select("svg")
+          .append("g")
+          .attr("id", "teamsG")
+            .attr("transform", "translate(50,100)")
+          .selectAll("g")
+          .data(incomingData)
+          .enter()
+          .append("g")
+            .attr("class", "overallG")
+            .attr("transform", (d, i) =>`translate(${(i * 50)}, 0)`);
 
-      const teamG = d3.selectAll("g.overallG");
+        var teamG = d3.selectAll("g.overallG");
 
-      teamG
-        .append("circle")
-        .attr("r", 0)
-        .transition()
-          .delay((d,i) => i *100)
-          .duration(500)
-          .attr("r", 40)
-        .transition()
-          .duration(500)
-          .attr("r",20)
+        teamG
+          .append("circle").attr("r", 0)
+          .transition()
+            .delay((d,i) => i * 100)
+            .duration(500)
+            .attr("r", 40)
+          .transition()
+            .duration(500)
+            .attr("r", 20);
 
-      teamG
-        .append("text")
+        teamG
+          .append("text")
           .style("text-anchor", "middle")
           .attr("y", 30)
-          .text(d => d.team);
+              .text(d => d.team)
 
       teamG
         .on("mouseover", this.highlightRegions);
 
       teamG
-        .on("mouseout", ( )=> {
-          d3.selectAll("g.overallG").select("circle")
-          .classed("inactive", false)
-          .classed("active", false)
-        });
+        .on("mouseout",this.unHighlightRegions);
   }
 
-  highlightRegions(d){
-    d3.selectAll("g.overallG").select("circle")
-      .attr("class", p => p.region === d.region ? "active" : "inactive");
+  highlightRegions(d, i){
+    d3.select(this).select("text").classed("active", true).attr("y", 10);
+       d3.selectAll("g.overallG").select("circle")
+          .each(function (p) {
+            p.region == d.region ?
+             d3.select(this).classed("active", true) :
+             d3.select(this).classed("inactive", true);
+          });
+    this.parentElement.appendChild(this);
+  }
 
+  unHighlight() {
+    d3.selectAll("g.overallG").select("circle").attr("class", "");
+    d3.selectAll("g.overallG").select("text")
+      .classed("active", false).attr("y", 30);
   }
 
   buttonClick(datapoint) {
