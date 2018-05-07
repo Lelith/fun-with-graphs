53ae6cc899741c7acb21dabafb6948ac392c9e9a
Kathrin Holzmann <lelith@gmx.de>
2018-05-07 16:56:50 +0200

    draw colored stacked areas

--- src/components/FunGraph.js
+++ src/components/FunGraph.js
@@ -1,84 +1,81 @@
 import React, { Component } from 'react';
 import * as d3 from "d3";
 
-
-export default class extends Component {
+export default class FunGraph extends Component {
+  constructor(props) {
+    super(props);
+    this.simpleStacking = this.simpleStacking.bind(this);
+  }
 
   componentDidMount() {
-    d3.csv("data/tweetdata.csv", this.dataViz);
+    d3.csv("data/movies.csv", (error, data) => {
+      if(error){
+        console.log(error);
+        return;
+      } else {
+        this.dataViz(data);
+      }
+    });
   }
 
   dataViz(data) {
-    const blue = "#5eaec5", green = "#92c463", orange = "#fe9a22";
+    const fillScale = d3.scaleOrdinal()
+      .domain(["titanic", "avatar", "akira", "frozen", "deliverance", "avengers"])
+      .range(["#fcd88a", "#cf7c1c", "#93c464", "#75734F", "#5eafc6", "#41a368"]);
 
-    const xScale = d3.scaleLinear().domain([1,10.5]).range([20,480]);
-
-    const yScale = d3.scaleLinear().domain([0,35]).range([480,20]);
+    const xScale = d3.scaleLinear().domain([ 1, 10 ]).range([ 20, 470 ]);
+    const yScale = d3.scaleLinear().domain([ 0, 55 ]).range([ 470, 20 ]);
 
     const xAxis = d3.axisBottom()
-     .scale(xScale)
-     .tickSize(480)
-     .tickValues([1,2,3,4,5,6,7,8,9,10]);
-
+      .scale(xScale)
+      .tickSize(480)
+      .tickValues([1,2,3,4,5,6,7,8,9,10]);
     const yAxis = d3.axisRight()
-     .scale(yScale)
-     .ticks(10)
-     .tickSize(480);
+      .scale(yScale)
+      .ticks(10)
+      .tickSize(470);
 
-     d3.select("svg").append("g").attr("id", "xAxisG").call(xAxis);
-     d3.select("svg").append("g").attr("id", "yAxisG").call(yAxis);
+    d3.select(this.refs.fungraph).append("g").attr("id", "xAxisG").call(xAxis);
 
-     d3.select("svg").selectAll("circle.favorites")
-      .data(data)
-      .enter()
-      .append("circle")
-        .attr("class", "favorites")
-        .attr("r", 5)
-        .attr("cx", d => xScale(d.day))
-        .attr("cy", d => yScale(d.favorites))
-        .style("fill", orange);
+    d3.select(this.refs.fungraph).append("g").attr("id", "yAxisG").call(yAxis);
 
-    d3.select("svg").selectAll("circle.tweets")
-      .data(data)
-      .enter()
-      .append("circle")
-       .attr("class", "tweets")
-       .attr("r", 5)
-       .attr("cx", d => xScale(d.day))
-       .attr("cy", d => yScale(d.tweets))
-       .style("fill", blue)
-
-  d3.select("svg").selectAll("circle.retweets")
-    .data(data)
-    .enter()
-    .append("circle")
-      .attr("class", "retweets")
-      .attr("r", 5)
-      .attr("cx", d => xScale(d.day))
-      .attr("cy", d => yScale(d.retweets))
-      .style("fill", green)
-
-  const lambdaXScale = d => xScale(d.day)
-
-  const tweetLine = d3.line()
-    .x(lambdaXScale)
-    .y(d => yScale(d.tweets));
-
-  d3.select("svg")
-    .append("path")
-    .attr("d", tweetLine(data))
-    .attr("fill", "none")
-    .attr("stroke", blue)
-    .attr("stroke-width", 2)
-
+    Object.keys(data[0]).forEach(key => {
+      if (key !== "day") {
+        var movieArea = d3.area()
+          .x(d => xScale(d.day))
+          .y0(d => yScale(this.simpleStacking(d,key) - d[key]))
+          .y1(d => yScale(this.simpleStacking(d, key)))
+          .curve(d3.curveBasis);
+        d3.select("svg")
+          .append("path")
+            .style("id", `${key} Area`)
+            .attr("d", movieArea(data))
+            .attr("fill", fillScale(key))
+            .attr("stroke", "black")
+            .attr("stroke-width", 1);
+      }
+    });
   }
 
+  simpleStacking( lineData, lineKey) {
+    var newHeight = 0;
+    Object.keys(lineData).every(key => {
+      if (key !== "day") {
+        newHeight += parseInt(lineData[key]);
+        if (key === lineKey) {
+          return false;
+        }
+      }
+      return true;
+    });
+    return newHeight;
+  }
 
 
   render() {
     return (
       <div>
-        <svg width="100%" height="100vh" ref="fungraph"/>
+        <svg width="600" height="600" ref="fungraph"/>
       </div>
     );
   }
